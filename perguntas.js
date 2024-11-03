9a38a2cee554521142a8a1bc91bd09dcf908be7a
BinaMoura <layanemouralay@gmail.com>
2024-11-03 19:47:42 -0300

    PROJETOFINAL

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'One Piece',
+    'Quem é o criador do anime One Piece?',
+    'One Piece foi criado por Eiichiro Oda.'
+)
+
+criaCartao(
+    'One Piece',
+    'Qual é o objetivo de Roronoa Zoro?',
+    'O objetivo de Zoro é se tornar o melhor espadachim do mundo.'
+)
+
+criaCartao(
+    'One Piece',
+    'Qual é o nome do "Rei dos Piratas" que inspirou Luffy a buscar o One Piece?',
+    'O nome do Rei dos Piratas é Gol D. Roger.'
+)
+
+criaCartao(
+    'One Piece',
+    'Qual é o nome da ilha onde o One Piece está supostamente escondido?',
+    'A ilha onde o One Piece está escondido é chamada Raftel (ou Laugh Tale).'
+)
